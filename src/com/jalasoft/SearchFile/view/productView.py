6521998ae9b536a425bebec5b8bd62ea4ad1746f
Julieta Escalera <julieta.escalera@jalasoft.com>
2019-03-08 11:07:51 -0400

    adding controller/integration

--- src/com/jalasoft/SearchFile/view/productView.py
+++ src/com/jalasoft/SearchFile/view/productView.py
@@ -11,22 +11,30 @@ class ProductView(QWidget):
         vLayout = QHBoxLayout()
         group = QGroupBox()
         form = QFormLayout()
+        self.pathText = QLineEdit()
 
-        form.addRow(QLabel("Path"), QLineEdit())
+        form.addRow(QLabel("Path"), self.pathText)
         form.addRow(QLabel("File Name"), QComboBox())
         form.addRow(QLabel("Ext"), QLineEdit())
         group.setLayout(form)
         buttons = QPushButton("Search")
-        table = QTableWidget()
-        table.setColumnCount(3)
-        table.setRowCount(1)
+        self.table = QTableWidget()
+        #self.table.setStyle("background-color: red")
+        self.table.setColumnCount(3)
+        self.table.setRowCount(1)
 
-        table.setHorizontalHeaderLabels(["path","file name","ext"])
-        table.setItem(0,0, QTableWidgetItem("c:'\'test"))
-        table.setItem(0,1, QTableWidgetItem("video.mp4"))
-        table.setItem(0,2, QTableWidgetItem("mp4"))
+        self.table.setHorizontalHeaderLabels(["path","file name","ext"])
+        # self.table.setItem(0,0, QTableWidgetItem("c:'\'test"))
+        # self.table.setItem(0,1, QTableWidgetItem("video.mp4"))
+        # self.table.setItem(0,2, QTableWidgetItem("mp4"))
 
         vLayout.addWidget(group)
         vLayout.addWidget(buttons)
-        vLayout.addWidget(table)
+        vLayout.addWidget(self.table)
         self.setLayout(vLayout)
+
+    def getTable(self):
+        return self.table
+
+    def getPath(self):
+        return self.pathText.text()
