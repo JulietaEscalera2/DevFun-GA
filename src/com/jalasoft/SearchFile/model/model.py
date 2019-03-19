79626486dc427e15cf3178d00252f7832f765054
teresalopez <teresa.lopez.e@gmail.com>
2019-03-19 09:26:00 -0400

    update model with objectParameters from controller2

--- src/com/jalasoft/SearchFile/model/model.py
+++ src/com/jalasoft/SearchFile/model/model.py
@@ -6,26 +6,36 @@ class Model():
 
     def __init__(self, objectParameters):
         self.objectParameters = objectParameters
-        print(objectParameters['Filename'])
         self.__name_file__ = self.objectParameters['Filename']
         self.__path_file__ = self.objectParameters['Path']
         self.__file_type__ = self.objectParameters['Extension']
         self.__file_size__ = self.objectParameters['Size']
         self.__file_creation_date__ = self.objectParameters['DateCreation']
 
+        result = {'Path': '', 'Filename': '', 'Extension': '', 'DateCreation': '', 'Size': ''}
+
     def search_criteria(self):
         result = []
+        searchParameters = {'Path': 'D:\\', 'Filename': 'vmware', 'Extension': '.log', 'DateCreation': 'Null',
+                            'Size': '1059442'}
 
         for root, dir, files in os.walk(self.__path_file__):
+
             for file in files:
-                if self.__name_file__!= " " and self.__name_file__ not in file.lower():
+                file_object = File(file, root)
+
+                if self.__name_file__!= 'Null' and self.__name_file__ not in file.lower():
                     continue
-                if self.__file_type__!= " " and not file.endswith(self.__file_type__):
+
+                if self.__file_type__!= 'Null' and not file.endswith(self.__file_type__):
                     continue
-                print("add to list")
-                print(self.__file_type__)
-                print(file)
-                result.append([root, file, self.__file_type__, self.__file_size__])
+
+                if self.__file_size__!= 'Null' and str(file_object.get_size_kb())!= str(self.__file_size__):
+                    continue
+                if self.__file_creation_date__!= 'Null' and file_object.get_creation_date()!= self.__file_creation_date__:
+                    continue
+
+                result.append([root, file, file_object.get_file_type(), file_object.get_size_kb()])
         return result
 
 
@@ -33,62 +43,49 @@ class Model():
     def search_all(self):
         path = self.__path_file__
         name = self.__name_file__
-        total = 0
         result = []
         for root, dir, files in os.walk(path):
             for file in files:
                 if name in file.lower():
-                    print(os.path.join(root, file))
-                    total += 1
                     file_object = File(file,root)
                     file_size = file_object.get_size_kb()
                     file_time = file_object.get_creation_date()
                     file_type = file_object.get_file_type()
                     result.append([root, file, file_size, file_time,file_type])
 
-        print("In total there are", total, " files with filename:", name, ",in the path:", path)
         return result
 
     # Search by file type/ext and path
     def search_by_type(self):
         path = self.__path_file__
         type = self.__file_type__
-        total = 0
         result = []
 
         for root, dirs, files in os.walk(path):
             for file in files:
                 if file.endswith(type):
-                    print(os.path.join(root, file))
-                    total += 1
                     file_object = File(file, root)
                     file_size = file_object.get_size_kb()
                     file_time = file_object.get_creation_date()
                     file_type = file_object.get_file_type()
                     result.append([root, file, file_size, file_time, file_type])
 
-        print("In total there are", total, " files with", type, "in", path)
         return result
 
     # Search by file size and path
     def search_by_size(self):
         path = self.__path_file__
         size = self.__file_size__
-        total = 0
         result = []
         for root, dirs, files in os.walk(path):
             for file in files:
                 file_object = File(file, root)
                 if os.stat(file_object.get_file_in_path()).st_size == size:
-                    print(os.path.join(root, file))
-                    total += 1
-
                     file_size = file_object.get_size_kb()
                     file_time = file_object.get_creation_date()
                     file_type = file_object.get_file_type()
                     result.append([root, file, file_size, file_time, file_type])
 
-        print("In total there are", total, " files with", size, "in", path)
         return result
 
 
